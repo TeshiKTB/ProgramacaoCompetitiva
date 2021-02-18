b3d4c83c207c52842e87fd73ebf85a94595c69c0
Teshi Tsurumi <teshi.kaue@gmail.com>
2021-02-18 19:46:27 -0300

    Added series.cpp

---
+++ Math/series.cpp
@@ -0,0 +1,12 @@
+
+// Soma dos n primeiros naturais
+S = 1 + 2 + 3 + ... + n = n * (n + 1) / 2
+
+// Soma dos quadrados dos n primeiros naturais:
+S = 1^2 + 2^2 + 3^2 + ... + n^2 = n * (n + 1) * (2 * n + 1) / 6
+
+// Soma dos cubos dos n primeiros naturais
+S = (n * (n + 1) / 2) ^ 2
+
+// Soma dos n primeiros impares
+S = 1 + 3 + 5 + ... + (2n + 1) = n ^ 2
